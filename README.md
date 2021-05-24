2b1903f4981c8e952cd3aaf767557a8397e15d26
Dimitri Belopopsky <dimitri@belopopsky.com>
2021-05-24 21:11:14 +0200

    Update README and add some more (failing) tests

--- README.md
+++ README.md
@@ -2,12 +2,6 @@
 
 ## Usage (WIP)
 
-You can run the emulator and test it against nestest by running without argument
-
-```
-cargo run
-```
-
 You can pass a ROM, but it won't do anything interesting for now.
 
 ```
@@ -22,17 +16,30 @@ cd nessy-rs
 cargo build
 ```
 
+## Testing
+
+There is an attempt to test as much as possible.
+Instructions and some parts of the emulator are being unit-tested.
+Some NES test roms are being used (notably nestest.nes).
+
+```
+cargo test
+```
+
 ## 6502 NES CPU
 
 Passes [nestest.nes](https://wiki.nesdev.com/w/index.php/Emulator_tests?source=post_page) (99.9%)
-Still missing a few unofficial instructions though...
-No cycle or timing handling for now.
+(Since there is no APU and PPU, some things are still missing at the end of the ROM).
+
+Still missing a few unofficial instructions...
+No cycle or timing handling for now. Cycle seems to be correct according to nestest though, so hopefully shouldn't be too difficult to setup accurately.
 Right now no ambition to make a full 6502 CPU, just NES one.
 
 ## Supported Features
 
 - Load ROMS
 
-### Mappers
+### Support mappers
 
 - Mapper 0
+- Mapper 1 (very WIP)
--- src/test_cpu.rs
+++ src/test_cpu.rs
@@ -1,55 +1,51 @@
-#[cfg(test)]
-mod cpu_test {
-    use crate::{nes_rom, nessy::Nessy};
 
-    #[test]
-    fn instr_test_v5_offical() {
-        let mut nessy = Nessy::new();
-        let nestest =
-            include_bytes!("../test_roms/instr_test-v5/official_only.nes");
+use crate::{nes_rom, nessy::Nessy};
 
-        // Load ROM and decode header
-        let rom = nestest;
-        let nesfile = nes_rom::RomFile::new(rom);
+#[test]
+fn instr_test_v5_offical() {
+    let mut nessy = Nessy::new();
+    let nestest = include_bytes!("../test_roms/instr_test-v5/official_only.nes");
 
-        nessy.load(&nesfile);
+    // Load ROM and decode header
+    let rom = nestest;
+    let nesfile = nes_rom::RomFile::new(rom);
 
-        loop {
-            nessy.execute();
-        }
+    nessy.load(&nesfile);
+
+    loop {
+        nessy.execute();
     }
-
-    #[test]
-    fn instr_misc() {
-        let mut nessy = Nessy::new();
-        let nestest =
-            include_bytes!("../test_roms/instr_misc/instr_misc.nes");
-
-        // Load ROM and decode header
-        let rom = nestest;
-        let nesfile = nes_rom::RomFile::new(rom);
-
-        nessy.load(&nesfile);
-
-        loop {
-            nessy.execute();
-        }
-    }
-
-    // #[test]
-    // fn cpu_exec_space() {
-    //     let mut nessy = Nessy::new();
-    //     let nestest =
-    //         include_bytes!("../test_roms/cpu_exec_space/test_cpu_exec_space_ppuio.nes");
-
-    //     // Load ROM and decode header
-    //     let rom = nestest;
-    //     let nesfile = nes_rom::RomFile::new(rom);
-
-    //     nessy.load(&nesfile);
-
-    //     loop {
-    //         nessy.execute();
-    //     }
-    // }
 }
+
+#[test]
+fn instr_misc() {
+    let mut nessy = Nessy::new();
+    let nestest = include_bytes!("../test_roms/instr_misc/instr_misc.nes");
+
+    // Load ROM and decode header
+    let rom = nestest;
+    let nesfile = nes_rom::RomFile::new(rom);
+
+    nessy.load(&nesfile);
+
+    loop {
+        nessy.execute();
+    }
+}
+
+// #[test]
+// fn cpu_exec_space() {
+//     let mut nessy = Nessy::new();
+//     let nestest =
+//         include_bytes!("../test_roms/cpu_exec_space/test_cpu_exec_space_ppuio.nes");
+
+//     // Load ROM and decode header
+//     let rom = nestest;
+//     let nesfile = nes_rom::RomFile::new(rom);
+
+//     nessy.load(&nesfile);
+
+//     loop {
+//         nessy.execute();
+//     }
+// }
